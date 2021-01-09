8590790a9d1b3724e94afb93ad50b7c1f25d2f27
Floor <floorvanwieringen@macbook-pro.lan>
2021-01-09 21:01:11 +0100

    Eerste commit

---
+++ end.js
@@ -0,0 +1,30 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+const MAX_HIGH_SCORES = 5;
+finalScore.innerText = mostRecentScore;
+username.addEventListener("keyup", function () {
+    console.log(username.value);
+    saveScoreBtn.disabled = !username.value;
+});
+
+function saveHighScore(e) {
+    console.log("clicked the save button");
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score);   
+    highScores.splice(5); 
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("highScores.html");
+    console.log(highScores);
+};
